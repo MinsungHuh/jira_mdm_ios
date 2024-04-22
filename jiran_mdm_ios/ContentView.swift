58b60f49adbf957c1cefe647424cfcded9cc4707
Minsung Huh <guitar9224@gmail.com>
2024-04-22 16:41:39 +0900

    Initial Commit

---
+++ jiran_mdm_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jiran_mdm_ios
+//
+//  Created by Minsung Huh on 4/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jiran_mdm_ios/jiran_mdm_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  jiran_mdm_iosApp.swift
+//  jiran_mdm_ios
+//
+//  Created by Minsung Huh on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct jiran_mdm_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
